78060b1d424405defe29dbf8ba8b4cc06bcdbf43
Eva Bergold <evabergold@gmail.com>
2024-04-15 10:51:18 +0200

    solved toggle function for bookmark and answer button

--- js/index.js
+++ js/index.js
@@ -1,4 +1,27 @@
-const showAnswer = document.querySelector("[data-js=answer-button]");
+const bookmarkElement = document.querySelector("[data-js=bookmark]");
+const answerButton = document.querySelector("[data-js=answer-button]");
+const cardAnswer = document.querySelector("[data-js=card-answer]");
+
+bookmarkElement.addEventListener("click", () => {
+  console.log("Bookmark clicked");
+
+  bookmarkElement.classList.toggle("fa-regular");
+  bookmarkElement.classList.toggle("fa-solid");
+});
+
+answerButton.addEventListener("click",()=>{
+
+console.log("answer button clicked");
+console.log(cardAnswer);
+
+cardAnswer.classList.toggle("hidden");
+
+if (cardAnswer.classList.contains("hidden")) {
+    answerButton.textContent = "Antwort zeigen";
+  } else {
+    answerButton.textContent = "Antwort verbergen";
+  }
 
 
-showAnswer.addEventListener ("click", ()=> {showAnswer.classList.add ("card__button__clicked")})
+});
+
